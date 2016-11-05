6471bada6ccfd5e88eccf3ec453aee551d78d14b
Jeremy Heyl <heyl@phas.ubc.ca>
2016-11-04 18:26:21 -0700

    m87 figure

--- generateregions.py
+++ generateregions.py
@@ -1,19 +1,11 @@
 
 output='''
 # Region file format: DS9 version 4.1
-global color=green dashlist=8 3 width=1 font="helvetica 10 normal roman" select=1 highlite=1 dash=0 fixed=0 edit=1 move=1 delete=1 include=1 source=1
-fk5
-panda(12:30:49.404,+12:23:28.11,30.2166,90,1,12",24",1) || # panda=(30.2166 90 180 270 189.968)(12" 24")
-panda(12:30:49.404,+12:23:28.11,90,180,1,12",24",1) || # panda=ignore
-panda(12:30:49.404,+12:23:28.11,180,270,1,12",24",1) || # panda=ignore
-panda(12:30:49.404,+12:23:28.11,270,549.968,1,12",24",1) || # panda=ignore
-panda(12:30:49.404,+12:23:28.11,210.217,450,1,12",24",1) # panda=(210.217 90 180 270 9.968)(12" 24")
-panda(12:30:49.404,+12:23:28.11,90,180,1,12",24",1) # panda=ignore
-panda(12:30:49.404,+12:23:28.11,180,270,1,12",24",1) # panda=ignore
-panda(12:30:49.404,+12:23:28.11,270,369.968,1,12",24",1) # panda=ignore
+panda(12:30:49.404,+12:23:28.11,30.2166,169.968,1,12",24",1) 
+panda(12:30:49.404,+12:23:28.11,210.217,369.968,1,12",24",1) 
 '''
 
-for minval in range(36,80):
+for minval in range(1,70):
     maxval=minval+1
     with open('isoregion_%02d.reg' % minval,'w') as f:
         f.write((output.replace('12"',str(minval)+'"')).replace('24"',str(maxval)+'"'))
--- m87.sh
+++ m87.sh
@@ -1,22 +1,27 @@
+# BSKYSUBT=   -3.897937895659532 / Amount of sky subtracted by WFC3 pipeline      
+# EXPTIME =               5599.0 / exposure duration (seconds)--calculated
+# BSKYSUBT*1.5*EXPTIME = -3.897937895659532/5599*1.5 = -0.001044276986
 ctioga2 --name m87 --line-style no \
 	--legend-inside lt:0.05,0.35 \
 	/scale 1.2 \
 	--xlog \
 	--xrange 0:2.5 --yrange 27:19 \
 	-l 'Global Hernquist Model' \
-	--yoffset 7 \
+	--yoffset 7.9 \
 	--line-style auto \
 	--marker none \
 	hern87.dat \
 	-l 'Central Hernquist Model' \
-	--yoffset 6.5 \
-	--xscale 0.4 \
+	--yoffset 6.65 \
+	--xscale 0.45 \
 	hern87.dat \
 	--line-style no \
 	--marker auto \
 	-l 'Observed WFC3' \
 	--yoffset 0 \
 	--xscale 1 \
-	photo.dat@'$2+0.5:-2.5*log10(($8+0.0011)/192.6)+10:yerr=1/sqrt(($8+0.0011)/1.5*5599*$11)' \
+	photo.dat@'$2+0.5:-2.5*log10(($8+0.0012)/192.6)+10:yerr=1/sqrt(($8+0.0012)/1.5*5599*$11)' \
+	-l 'Observed WFC3 (8\% less sky)' \
+	photo.dat@'$2+0.5:-2.5*log10(($8+0.0013)/192.6)+10:yerr=1/sqrt(($8+0.0013)/1.5*5599*$11)' \
 	-x 'Radius [Arcseconds]' \
 	-y '$\mu_\mathrm{F275W}$'
